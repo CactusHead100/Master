9c43fefeb088d66308543e4c8e6a0d2844108419
CactusHead100 <cactushead100@gmail.com>
2023-05-12 12:44:09 +1200

    Added class player with variables for both it and its hammmer

--- Game.js
+++ Game.js
@@ -4,4 +4,22 @@ window.onload=() => {
 
     var canvasWidth = 800
     var canvasHeight = 600
+//player object with x,y,width,height,velocities and also similar properties for its hammer
+    class Player{
+        constructor(){
+            this.x = 0
+            this.y = 0
+            this.width = 0
+            this.height = 0
+            this.xVelocity = 0
+            this.yVelocity = 0
+
+            this.hammer = {
+                x:0,
+                y:0,
+                width:0,
+                height:0,
+            }
+        }
+    }
 }
